fdf1620802f625f74d57a99479a8eb20630536fa
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-23 08:50:37 +0530

    Update Alert.js

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -5,7 +5,7 @@ export default function Alert(props) {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }
   return (
-    <div style={{ height: '48px' }} className='al mb-2'>
+    <div style={{ height: '48px', marginTop:"4.1rem" }} className='al mb-2'>
       {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{Cap(props.alert.type)}</strong>:{props.alert.msg}
       </div>}
